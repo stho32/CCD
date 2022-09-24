96397fe61d9924f7a973333986241ec5f08db85e
Stefan Hoffmann <stho32github@gmail.com>
2022-09-24 19:36:30 +0200

    Es gibt einen neuen InMemoryTextOutput, damit bei Tests die Ausgabe aus dem Speicher geprüft werden kann.

---
+++ Source/ccd-course/csvviewer.BL.Tests/Mocks/InMemoryTextOutput.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using csvviewer.Interfaces;
+
+namespace csvviewer.BL.Tests.Mocks;
+
+public class InMemoryTextOutput : IOutput
+{
+    private StringBuilder _result = new(); 
+
+    public void WriteLine(string text)
+    {
+        _result.AppendLine(text);
+    }
+
+    public void Write(string text)
+    {
+        _result.Append(text);
+    }
+
+    public string GetResult()
+    {
+        return _result.ToString();
+    }
+}
